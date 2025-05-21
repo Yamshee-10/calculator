23336294d5aa72ec2ca72730b4c3b645a7993390
Yamshee Ajaz <166216600+Yamshee-10@users.noreply.github.com>
2025-05-21 12:47:01 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// let displayValue = '';
+// let btn1 = document.querySelector('#1');
+// let btn2 = document.querySelector('#2');
+// let btn3 = document.querySelector('#3');
+// let btn4 = document.querySelector('#4');
+// let btn5 = document.querySelector('#5');
+// let btn6 = document.querySelector('#6');
+// let btn7 = document.querySelector('#7');
+// let btn8 = document.querySelector('#8');
+// let btn9 = document.querySelector('#9');
+// let btn0 = document.querySelector('#0');
+// let btnPlus = document.querySelector('#+');
+// let btnMinus = document.querySelector('#-');
+// let btnMultiply = document.querySelector('#*');
+// let btnDivide = document.querySelector('#/');
+// let btnClear = document.querySelector('#C');
+// let btnEqual = document.querySelector('#=');
+
+// // document.querySelector('#displayVal').value = displayValue;
+// btn1.onclick(){
+//     displayValue = displayValue + '1';
+// }
+
